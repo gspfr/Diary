1d4ad0b14ca29ba7daa99118504b70dc12fdba3e
gabriel <gabrielsautiere@gmail.com>
2023-12-09 16:11:45 +0100

    a

--- main.c
+++ main.c
@@ -3,7 +3,23 @@
 
 int main() {
 
-    createContactList();
+    
+
+    /*for (int i = 0; i<list.max_lvl;i++) {
+        contact *temp = list.heads[i];
+        printf("level %d\n", i);
+        int j = 0;
+        while (temp != NULL) {
+            printf("%d %s_%s %d niveaux\n",j, temp->surname, temp->firstname, temp->nb_level);
+            temp = temp->nexts[i];
+            j++;
+        }
+        printf("\n");
+    }*/
+
+
+
+    //createContactList();
     //menu();
 
 
